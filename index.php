c17c9b590934fd8a573602e01e7ec8a59fa2c6c3
Kuba OLSZAK <analogband@gmail.com>
2023-05-09 12:37:27 +0200

    GET 'name' content

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
 <?php
 
-$word = "hello";
+$name;
 
-echo "$word <br>";
-echo ucfirst($word);
+if(isset($_GET['firstname']) && !empty($_GET['firstname'])) {
+  $name = $_GET['firstname'];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+  <body>
+    <form action="" method="GET">
+      <input type="text" name="firstname">
+      <input type="submit" value="Send">
+    </form>
+    <p>
+      Name : <?= $name; ?>
+    </p>
+</body>
+</html>
