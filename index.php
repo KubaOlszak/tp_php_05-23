0c172e4fa2f69d60658531ac6cd9893fc8f6f965
Kuba OLSZAK <analogband@gmail.com>
2023-05-09 23:23:53 +0200

    exo 17 - Triangle de nombres

--- index.php
+++ index.php
@@ -1,23 +1,12 @@
 <?php
 
-$i = 1;
-$prime = '';
-
-while ($i <= 5) {
-  for ($j = 2; $j <= $i; $j++) {
-    if ($i % $j === 0) {
-      return false;
-    } else {
-    echo $i . ", ";
+function triangle($n) {
+  for ($i = 1; $i <= $n ; $i++) {
+    for ($j = 1; $j <= $i; $j++){
+      echo $i;
     }
+    echo '<br>';
   }
-  echo "$i<br>";
-  $i++;
 }
 
-// var_dump($prime);
-
-function prime ()
-{
-
-} 
+triangle(5);
