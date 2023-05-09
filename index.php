074a2ba0eec73fe207cc6f9ee6332c2ebe27eb3b
Kuba OLSZAK <analogband@gmail.com>
2023-05-09 14:18:05 +0200

    Puissance corrigée

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <?php
 
 $a = 2;
-$b = -3;
+$b = 3;
 
 echo "a : $a <br>";
 echo "b : $b <br><br>";
 
-echo "a^b = " . $a**$b . "<br>";
-echo "ou<br>";
-echo "a^b = " . pow($a, $b) . "<br>";
+if (!(is_int($a)) ||  !(is_int($b))) {
+  echo "Les variables doivent être des nombres.<br>";
+} else {
+  echo "a^b = " . $a**$b . "<br>";
+}
