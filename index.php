67933ea509c42c866f0b9a0b5078ddca87988a5e
Kuba OLSZAK <analogband@gmail.com>
2023-05-09 12:20:10 +0200

    First letetr uppercase

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 
-$stringTp = "Ceci est une chaîne de caractère.";
+$word = "hello";
 
-$substr = "est une chaîne";
-
-$substrLength = strlen($substr);
-
-$pos = strpos($stringTp, $substr);
-
-if (str_contains($stringTp, $substr)) {
-    echo substr($stringTp, $pos, $substrLength);
-} else {
-  echo "Cette chaine n'est pas dans cette phrase.";
-}
+echo "$word <br>";
+echo ucfirst($word);
