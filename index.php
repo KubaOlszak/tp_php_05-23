de104692cd3dd4dcd651f828371e74ffb2e35c68
Kuba OLSZAK <analogband@gmail.com>
2023-05-09 10:12:12 +0200

    commit about movies

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-echo "Hello world!";
+
+$movieArray = [
+  "Vertigo",
+  "Minority Report",
+  "Blade Runner",
+  "Le Dernier des Mohicans",
+  "Pacific Rim"
+];
+
+echo "<ul>";
+foreach ($movieArray as $movie) {
+  echo "<li>$movie</li>";
+}
+echo "</ul>";
